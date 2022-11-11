7261fc79c6ccb0d27ab3c09863c75c7358306f8f
Vinh Tho Hoang <vinhtho.hoang@gmail.com>
2022-11-11 10:49:31 -0500

    watchlist

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # # 1) A user can see all the lists - GET "lists" (INDEX)
+  # get 'lists', to: 'lists#index'
+  # # 2) A user can see the details of a given list and its name - GET "lists/42" (SHOW)
+  # get 'lists/:id', to: 'lists#show'
+  # # 3) A user can create a new list - GET "lists/new" - POST "lists" (CREATE)
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+
+  # A user can delete a bookmark from a list. How can we make a delete link again?
+  # DELETE "bookmarks/25"
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
