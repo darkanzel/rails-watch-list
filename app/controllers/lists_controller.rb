d5351474330bf5ca44da23f2f0841e18b1cf6f1d
Vinh Tho Hoang <vinhtho.hoang@gmail.com>
2022-11-11 15:52:54 -0500

    watch list green controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,13 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    # @list = List.find(params[:id]) (in set_list)
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -15,15 +17,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to @list, notice: "List was successfully created."
+      redirect_to list_path(@list), notice: "List was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
